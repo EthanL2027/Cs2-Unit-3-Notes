531e97bc6650a51000093b581a25b3f774f45ccf
Ethan <Ethan.lynn2027@gbwl.org>
2025-12-12 13:30:49 +0000

    3.4 Notes - Begining

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
 
       // All objects can be created using this pattern:
       // ClassName objectName = new ClassName();
-      BuildABear hero = new BuildABear ("ironman");
+      BuildABear hero = new BuildABear ();
 
    }
 }
